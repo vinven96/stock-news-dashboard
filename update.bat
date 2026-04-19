f34c0163ff790652da705442aaeff2c53b6244eb
Vinay <vinven96@gmail.com>
2026-04-19 09:17:50 -0400

    hourly update

--- update.bat
+++ update.bat
@@ -1,6 +1,8 @@
 @echo off
-cd /d C:\AIProjects\stock_news_agent
+cd /d C:\AIProjects\stock_news_agent_code
 python stock_news_agent.py
+copy C:\AIProjects\stock_news_agent_code\index.html C:\AIProjects\stock_news_agent\
+cd /d C:\AIProjects\stock_news_agent
 git add .
 git diff --cached --quiet || (
     git commit -m "hourly update"
